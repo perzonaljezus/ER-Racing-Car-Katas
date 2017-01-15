ce655bccff30abb2944e15ce90346aec17f54fef
Eduard Ruffert At Home <ruffert@gmail.com>
2017-01-15 12:13:41 +0100

    adds initial empty failing test

--- java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
+++ java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,7 +1,18 @@
 package tddmicroexcersises.tirepressuremonitoringsystem;
 
+import org.junit.Test;
+import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by pj on 15/01/17.
  */
 public class AlarmTest {
+
+    @Test
+    public void testAlarm() {
+        Alarm alarm = new Alarm();
+        assertTrue(alarm.isAlarmOn());
+    }
 }
