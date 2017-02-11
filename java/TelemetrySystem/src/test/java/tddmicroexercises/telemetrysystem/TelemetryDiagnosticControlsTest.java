af964ffb9500ccfd249ec578359d81ee2eca0d9c
Eduard Ruffert At Home <ruffert@gmail.com>
2017-02-11 11:25:12 +0100

    test checkTransmission
    If we run this test often (+10 times) we see a 20% failure rate, although we didn't change anything between the runs.
    
    Looking at the failiure, we see that since these are controls connected to a telemetry system sending live (simulated) values, we see that we never can be sure that the same value is retrieved in the diagnostic Info. So basically this is un-testable.
    But we know we had the same problem with the TyrePressure Excercise.
    
    Next we have to create the seam and stop accessing the real TelemetryClient.

--- java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -73,7 +73,24 @@ public class TelemetryDiagnosticControlsTest {
      */
     @Test
     public void testCheckTransmission() throws Exception {
-//TODO: Test goes here... 
+        String expected = "LAST TX rate................ 100 MBPS\r\n" //
+                + "HIGHEST TX rate............. 100 MBPS\r\n" //
+                + "LAST RX rate................ 100 MBPS\r\n" //
+                + "HIGHEST RX rate............. 100 MBPS\r\n" //
+                + "BIT RATE.................... 100000000\r\n" //
+                + "WORD LEN.................... 16\r\n" //
+                + "WORD/FRAME.................. 511\r\n" //
+                + "BITS/FRAME.................. 8192\r\n" //
+                + "MODULATION TYPE............. PCM/FM\r\n" //
+                + "TX Digital Los.............. 0.75\r\n" //
+                + "RX Digital Los.............. 0.10\r\n" //
+                + "BEP Test.................... -5\r\n" //
+                + "Local Rtrn Count............ 00\r\n" //
+                + "Remote Rtrn Count........... 00";
+
+        controls.checkTransmission();
+        String actual = controls.getDiagnosticInfo();
+        assertEquals(expected, actual);
     }
 
 
