e6829fe3769a294a9e5c4f78bd461bd35626c31d
Eduard Ruffert At Home <ruffert@gmail.com>
2017-03-16 21:25:47 +0100

    adds trest for too high pressure, which drives us to think how to do this.
    want a configurable TestableAlarm, so that we can pass a test pressure value.
    we change the constructor of TestableAlarm to take a parameter, which we return in pressureValue.
    
    and adjust the existing test.

--- java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
+++ java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by pj on 15/01/17.
@@ -12,14 +13,28 @@ public class AlarmTest {
 
     @Test
     public void testAlarmIsOffWhenPressureIsOk() {
-        Alarm alarm = new TestableAlarm();
+        Alarm alarm = new TestableAlarm(20.);
         alarm.check();
         assertFalse(alarm.isAlarmOn());
     }
+    @Test
+    public void testAlarmIsOnWhenPressureIsTooHigh() {
+        Alarm alarm = new TestableAlarm(22.);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
 
     private class TestableAlarm extends Alarm {
+        private double mockPressure;
+
+        public TestableAlarm(double mockPressure) {
+            super();
+
+            this.mockPressure = mockPressure;
+        }
+
         protected double pressureValue() {
-            return 20. ;
+            return mockPressure ;
         }
     }
 }
