5d494a02abd9954d8ed52bf4919821284d9adeb2
Eduard Ruffert At Home <ruffert@gmail.com>
2017-03-18 18:23:36 +0100

    our ok test looks quite fine, so add the next test, because we need to cover the domain.
    and add comments about the next test that we need

--- java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
+++ java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTest.java
@@ -8,6 +8,7 @@ import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
 import tddmicroexercises.tirepressuremonitoringsystem.SafetyRange;
 import tddmicroexercises.tirepressuremonitoringsystem.Sensor;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.BDDMockito.given;
 
@@ -23,11 +24,24 @@ public class AlarmTest {
 
     @Test
     public void testAlarmIsOffWhenPressureIsOk() {
-        double okPressure = safetyRange.midValue();
-        given(sensor.popNextPressurePsiValue()).willReturn(okPressure);
+        double pressure = safetyRange.midValue();
+        given(sensor.popNextPressurePsiValue()).willReturn(pressure);
 
         Alarm alarm = new Alarm(sensor, safetyRange);
         alarm.check();
         assertFalse(alarm.isAlarmOn());
     }
+    @Test
+    public void testAlarmIsOnWhenPressureIsTooHigh() {
+        double pressure = safetyRange.getHighPressureThreshold() + 1;
+        given(sensor.popNextPressurePsiValue()).willReturn(pressure);
+
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    // too low pressure
+    // pressure is exactly the low value, this allows us to understand the logic of Sensor i.e. whether the thresholds are inclusive or exclusive
+    // pressure is exactly the high value, dto.
 }
