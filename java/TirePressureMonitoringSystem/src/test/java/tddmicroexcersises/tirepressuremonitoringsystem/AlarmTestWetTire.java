8e78e0f936acd3584ea3c8413c390bc24cdeff6d
Eduard Ruffert At Home <ruffert@gmail.com>
2017-03-18 15:09:54 +0100

    we need to change AlarmTest to support parametrized class as well as mock runnner. I didn't find a solution, so I violate DRY and copy AlarmTest to the specific test class for wet tire adn rename AlarmTest to AlarmTestNormalTire.

---
+++ java/TirePressureMonitoringSystem/src/test/java/tddmicroexcersises/tirepressuremonitoringsystem/AlarmTestWetTire.java
@@ -0,0 +1,70 @@
+package tddmicroexcersises.tirepressuremonitoringsystem;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
+import tddmicroexercises.tirepressuremonitoringsystem.SafetyRange;
+import tddmicroexercises.tirepressuremonitoringsystem.SafetyRangeWetTire;
+import tddmicroexercises.tirepressuremonitoringsystem.Sensor;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+/**
+ * Created by pj on 15/01/17.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AlarmTestWetTire {
+    @Mock
+    private Sensor sensor;
+    private SafetyRange safetyRange;
+
+
+    @Before
+    public void setUp() throws Exception {
+        safetyRange = new SafetyRangeWetTire(SafetyRangeWetTire.lowPressureThreshold, SafetyRangeWetTire.highPressureThreshold);
+    }
+
+    @Test
+    public void testAlarmIsOffWhenPressureIsOk() {
+        double okPressure = (safetyRange.highPressureThreshold + safetyRange.lowPressureThreshold)/2;
+        given(sensor.popNextPressurePsiValue()).willReturn(okPressure);
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
+    @Test
+    public void testAlarmIsOnWhenPressureIsTooHigh() {
+        given(sensor.popNextPressurePsiValue()).willReturn(safetyRange.highPressureThreshold +1);
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+    @Test
+    public void testAlarmIsOnWhenPressureIsTooLow() {
+        given(sensor.popNextPressurePsiValue()).willReturn(safetyRange.lowPressureThreshold -1);
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+    @Test
+    public void testAlarmIsOffWhenPressureIsLowLimit() {
+        given(sensor.popNextPressurePsiValue()).willReturn(safetyRange.lowPressureThreshold);
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
+    @Test
+    public void testAlarmIsOffWhenPressureIsHighLimit() {
+        given(sensor.popNextPressurePsiValue()).willReturn(safetyRange.highPressureThreshold);
+        Alarm alarm = new Alarm(sensor, safetyRange);
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
+
+
+}
